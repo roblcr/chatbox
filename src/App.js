3c973b603efe71a46ee44be3182b9c30462a30a8
roblcr <lecullier.robin@gmail.com>
2021-10-27 17:23:39 +0200

    message save in state

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ import Message from './Components/Message'
 
 
 class App extends Component {
+  state = {
+    messages: {},
+    pseudo: this.props.match.params.pseudo
+  }
+
+  addMessage = message => {
+    const messages = {...this.state.messages}
+    messages[`message-${Date.now()}`] = message
+    this.setState({messages})
+  }
+
   render () {
     return (
       <div className='box'>
@@ -13,7 +24,9 @@ class App extends Component {
             <Message />
           </div>
         </div>
-        <Formulaire />
+        <Formulaire
+          pseudo={this.state.pseudo}
+         addMessage={this.addMessage}/>
         </div>
     )
   }
