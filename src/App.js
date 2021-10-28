72cecc4416ccaf48903b7bafdfd366a40989aecb
roblcr <lecullier.robin@gmail.com>
2021-10-28 10:26:56 +0200

    sending message and display

--- src/App.js
+++ src/App.js
@@ -17,10 +17,23 @@ class App extends Component {
   }
 
   render () {
+
+    const messages = Object
+    .keys(this.state.messages)
+    .map(key => (
+      <Message
+        key={key}
+        pseudo={this.state.messages[key].pseudo}
+        message={this.state.messages[key].message}
+      />
+    ))
     return (
       <div className='box'>
         <div>
           <div className="messages">
+            <div className="message">
+              { messages }
+            </div>
             <Message />
           </div>
         </div>
