5366b6c5f4ec89018a2f91b39531cac5c6722c29
roblcr <lecullier.robin@gmail.com>
2021-10-28 10:19:02 +0200

    message lenght and keyup event to send message

--- src/Components/Formulaire.js
+++ src/Components/Formulaire.js
@@ -4,11 +4,12 @@ class Formulaire extends Component {
 
 
     state = {
-        message: ''
+        message: '',
+        length: this.props.length
     }
 
     createMessage = () => {
-        const { addMessage, pseudo } = this.props
+        const { addMessage, pseudo, length } = this.props
 
         const message = {
             pseudo,
@@ -18,7 +19,7 @@ class Formulaire extends Component {
         addMessage(message)
 
         // Reset 
-        this.setState({ message: '' })
+        this.setState({ message: '', length })
     }
 
     
@@ -30,7 +31,14 @@ class Formulaire extends Component {
 
     handleChange = event => {
         const message = event.target.value
-        this.setState({message})
+        const length = this.props.length - message.length
+        this.setState({message, length})
+    }
+
+    handleKeyUp = event => {
+        if (event.key === 'Enter'){
+            this.createMessage()
+        }
     }
 
     render() {
@@ -40,11 +48,12 @@ class Formulaire extends Component {
                 <textarea 
                 value={this.state.message}
                 onChange={this.handleChange}
+                onKeyUp={this.handleKeyUp}
                 required
-                maxLength='140'>
+                maxLength={this.props.length}>
                 </textarea>
                 <div className='info'>
-                    140
+                    {this.state.length}
                 </div>
                 <button type='submit'>
                     Envoyer !
